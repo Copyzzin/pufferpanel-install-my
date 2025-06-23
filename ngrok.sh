c2ab38c55ad40f3628b8cf98b0a01a464060c2dc
Copyzzin <106496046+Copyzzin@users.noreply.github.com>
2025-06-23 00:09:04 -0300

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2XWvdIQegnz7fRUXkMvJWVy7Z5p_3SijirFvadrRgBJot32fG #Put Yours here
+./ngrok config add-authtoken 2iyaNipnGT6XH6lJ5iHmX9Z7ehZ_2Uk7h74T6TrbJA6ZsKQQk
 ./ngrok http 8080
